5e59f1462a2a0516ed4903688e2d479a7cfde7c0
Ethan Edmond <eedmond@uccs.edu>
2021-11-16 13:01:17 -0700

    Added TODO and some styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,8 @@ function Header() {
       <div className='content-wrapper'>
         <h1>Hello, I'm <em>Ethan Edmond</em>.</h1>
         <h2 className='bread-tagline'>I'm a</h2>
-	<h2 className='meat-tagline'>web developer/software engineer/problem solver</h2>
+	<h2 className='meat-tagline'>web developer</h2>
+	{/* TODO: replace meat with a little roulette/marquee thing rotating between web developer software engineer and problem solver maybe also do this where it says beautiful solutions */}
 	<h2 className='bread-tagline'>who loves finding beautiful solutions</h2>
       </div>
     </header>

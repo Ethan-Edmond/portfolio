b9c6aa1e7a28820cea7d6ba30268484ac3da664a
Ethan Edmond <eedmond@uccs.edu>
2021-11-16 12:25:29 -0700

    Made a header scss file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import './Header.scss';
+
 function Header() {
   return (
     <header className='hero-area' id='main-hero'>
       <div className='content-wrapper'>
         <h1>Hello, I'm <em>Ethan Edmond</em>.</h1>
-        <h2>I'm a student looking to start working in web development.</h2>
+        <h2 className='bread-tagline'>I'm a</h2>
+	<h2 className='meat-tagline'>web developer/software engineer/problem solver</h2>
+	<h2 className='bread-tagline'>who loves finding beautiful solutions</h2>
       </div>
     </header>
   );

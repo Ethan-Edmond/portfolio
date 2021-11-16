0579256335d9729385baf1c537628f6ada5bd45b
Ethan Edmond <eedmond@uccs.edu>
2021-11-16 15:04:33 -0700

    Moved header css from app to Header.scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,14 @@ import './Header.scss';
 function Header() {
   return (
     <header className='hero-area' id='main-hero'>
-      <div className='content-wrapper'>
-        <h1>Hello, I'm <em>Ethan Edmond</em>.</h1>
-        <h2 className='bread-tagline'>I'm a</h2>
-	<h2 className='meat-tagline'>web developer</h2>
-	{/* TODO: replace meat with a little roulette/marquee thing rotating between web developer software engineer and problem solver maybe also do this where it says beautiful solutions */}
-	<h2 className='bread-tagline'>who loves finding beautiful solutions</h2>
-      </div>
-    </header>
+       <div className='content-wrapper'>
+         <h1>Hello, I'm <em>Ethan Edmond</em>.</h1>
+         <h2 className='bread-tagline'>I'm a
+   	<span className='meat-tagline'> web developer </span>
+   	{ /* TODO: replace meat with a little roulette/marquee thing rotating between web developer software engineer and problem solver maybe also do this where it says beautiful solutions */ }
+   	who loves finding beautiful solutions</h2>
+       </div>
+     </header>
   );
 }
 
